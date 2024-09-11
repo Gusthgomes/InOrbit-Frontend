a3b5deedc1d18be6b15ad6c5a88a1ec0ba505674
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-09-11 20:18:29 -0300

    feat: home page created

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,26 @@
+import logo from "./assets/in-orbit-logo.svg";
+import letsStart from "./assets/rocket-launch-illustration.svg";
+
+import { Plus } from "lucide-react";
+
 export function App() {
   return (
-    <>
-      <h1 className="text-green-400 text-2xl">Que tal</h1>
-      <p>fazendo um teste apenas</p>
-    </>
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="logo" />
+      <img src={letsStart} alt="logo" />
+
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo
+        ?
+      </p>
+
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 flex items-center gap-2 text-sm font-medium -tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
   );
 }
